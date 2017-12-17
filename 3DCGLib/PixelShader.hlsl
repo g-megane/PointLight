f0ac111f59bc4e0e572604711be2aae221a48161
Ryota Goto <pandoraxssscan.929@gmail.com>
2017-12-18 03:03:29 +0900

    頂点シェーダ－とピクセルシェーダーを点光源に対応

--- 3DCGLib/PixelShader.hlsl
+++ 3DCGLib/PixelShader.hlsl
@@ -1,10 +1,43 @@
-struct VS_OUTPUT
+cbuffer ConstantBuffer : register(b0)
+{
+    matrix World;
+    matrix View;
+    matrix Projection;
+    float4 Light;
+    float4 Attenuation;
+}
+
+struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
-    float4 Color : COLOR0;
+    float4 PosW : POSITION0;
+    float4 NormalW : NORMAL0;
 };
 
-float4 PS(VS_OUTPUT input) : SV_Target
+float4 PS(PS_INPUT input) : SV_Target
 {
-    return input.Color;
+    float3 dir;
+    float len;
+    float colD;
+    float colA;
+    float col;
+
+    // “_ŒõŒ¹‚Ì•ûŒü
+    dir = Light.xyz - input.PosW.xyz;
+
+    // “_ŒõŒ¹‚Ì‹——£
+    len = length(dir);
+
+    // “_ŒõŒ¹‚Ì•ûŒü‚ğ³‹K‰»
+    dir = dir / len;
+
+    // ŠgU
+    colD = saturate(dot(normalize(input.NormalW.xyz), dir));
+
+    // Œ¸Š
+    colA = saturate(1.0f / (Attenuation.x + Attenuation.y * len + Attenuation.z * len * len));
+
+    col = colD * colA;
+
+    return float4(col, col, col, 1.0f);
 }
--- 3DCGLib/VertexShader.hlsl
+++ 3DCGLib/VertexShader.hlsl
@@ -3,25 +3,32 @@ cbuffer ConstantBuffer : register(b0)
     matrix World;
     matrix View;
     matrix Projection;
+    float4 Light;
+    float4 Attenuation;
 }
+
+struct VS_INPUT
+{
+    float4 Pos    : POSITION0;
+    float4 Normal : NORMAL0;
+};
 
 struct VS_OUTPUT
 {
-    float4 Pos : SV_POSITION;
-    float4 Color : COLOR0;
+    float4 Pos  : SV_POSITION;
+    float4 PosW : POSITION0;
+    float4 Normalw : NORMAL0;
 };
 
-VS_OUTPUT VS(float4 Pos : POSITION, float4 Color : COLOR)
+VS_OUTPUT VS(VS_INPUT input)
 {
-    VS_OUTPUT output = (VS_OUTPUT)0;
-    output.Pos = mul(Pos, World);
-    output.Pos = mul(output.Pos, View);
-    output.Pos = mul(output.Pos, Projection);
-    output.Color = Color;
+    VS_OUTPUT output;
+
+    output.PosW = mul(input.Pos, World);
+    output.Pos  = mul(output.PosW, View);
+    output.Pos  = mul(output.Pos, Projection);
+
+    output.Normalw = mul(input.Normal, World);
+
     return output;
 }
-
-//float4 PS(VS_OUTPUT input) : SV_Target
-//{
-//    return input.color;
-//}
